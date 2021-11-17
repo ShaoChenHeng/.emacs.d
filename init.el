a76c4266ac5955691842a4d6bacfa9f257ebf85d
shaochenheng <1010716589@qq.com>
2021-11-17 09:13:04 +0000

    Update init.el

--- init.el
+++ init.el
@@ -13,6 +13,8 @@
 ;; eaf
 (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/app/file-browser/")
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/app/eaf-pdf-viewer/")
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/app/eaf-netease-cloud-music/")
 
 (setq eaf-proxy-type "socks5")
 (setq eaf-proxy-host "127.0.0.1")
@@ -24,6 +26,10 @@
 (require 'eaf-file-manager)
 (require 'eaf-file-browser)
 (require 'eaf-vue-demo)
+(require 'eaf-file-browser)
+(require 'eaf-netease-cloud-music)
+(require 'eaf-org-previewer)
+(require 'eaf-pdf-viewer)
 
 ;; elfeed
 (add-to-list 'load-path "~/.emacs.d/site-lisp/elfeed")
@@ -35,3 +41,55 @@
 ;; use nano theme
 ;; (add-to-list 'load-path "~/.emacs.d/site-lisp/nano-emacs")
 ;; (require 'nano)
+
+;; awesome-pair
+(add-to-list 'load-path "~/.emacs.d/site-lisp/awesome-pair")
+(require 'awesome-pair)
+(dolist (hook (list
+               'c-mode-common-hook
+               'c-mode-hook
+               'c++-mode-hook
+               'java-mode-hook
+               'haskell-mode-hook
+               'emacs-lisp-mode-hook
+               'lisp-interaction-mode-hook
+               'lisp-mode-hook
+               'maxima-mode-hook
+               'ielm-mode-hook
+               'sh-mode-hook
+               'makefile-gmake-mode-hook
+               'php-mode-hook
+               'python-mode-hook
+               'js-mode-hook
+               'go-mode-hook
+               'qml-mode-hook
+               'jade-mode-hook
+               'css-mode-hook
+               'ruby-mode-hook
+               'coffee-mode-hook
+               'rust-mode-hook
+               'qmake-mode-hook
+               'lua-mode-hook
+               'swift-mode-hook
+               'minibuffer-inactive-mode-hook
+               ))
+  (add-hook hook '(lambda () (awesome-pair-mode 1))))
+
+(define-key awesome-pair-mode-map (kbd "(") 'awesome-pair-open-round)
+(define-key awesome-pair-mode-map (kbd "[") 'awesome-pair-open-bracket)
+(define-key awesome-pair-mode-map (kbd "{") 'awesome-pair-open-curly)
+(define-key awesome-pair-mode-map (kbd ")") 'awesome-pair-close-round)
+(define-key awesome-pair-mode-map (kbd "]") 'awesome-pair-close-bracket)
+(define-key awesome-pair-mode-map (kbd "}") 'awesome-pair-close-curly)
+(define-key awesome-pair-mode-map (kbd "%") 'awesome-pair-match-paren)
+(define-key awesome-pair-mode-map (kbd "\"") 'awesome-pair-double-quote)
+(define-key awesome-pair-mode-map (kbd "M-o") 'awesome-pair-backward-delete)
+(define-key awesome-pair-mode-map (kbd "C-k") 'awesome-pair-kill)
+(define-key awesome-pair-mode-map (kbd "M-\"") 'awesome-pair-wrap-double-quote)
+(define-key awesome-pair-mode-map (kbd "M-[") 'awesome-pair-wrap-bracket)
+(define-key awesome-pair-mode-map (kbd "M-{") 'awesome-pair-wrap-curly)
+(define-key awesome-pair-mode-map (kbd "M-(") 'awesome-pair-wrap-round)
+(define-key awesome-pair-mode-map (kbd "M-)") 'awesome-pair-unwrap)
+(define-key awesome-pair-mode-map (kbd "M-p") 'awesome-pair-jump-right)
+(define-key awesome-pair-mode-map (kbd "M-n") 'awesome-pair-jump-left)
+(define-key awesome-pair-mode-map (kbd "M-:") 'awesome-pair-jump-out-pair-and-newline)
