3c0bc39e053299f875ef83e93bc5fd30929056d3
shaochenheng <1010716589@qq.com>
2021-11-24 09:22:56 +0000

    Add files

---
+++ lisp/init-eaf.el
@@ -0,0 +1,21 @@
+;; eaf
+(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/app/file-browser/")
+
+
+(setq eaf-proxy-type "socks5")
+(setq eaf-proxy-host "127.0.0.1")
+(setq eaf-proxy-port "1080")
+(require 'eaf)
+(require 'eaf-browser)
+(require 'eaf-rss-reader)
+(require 'eaf-music-player)
+(require 'eaf-file-manager)
+(require 'eaf-file-browser)
+(require 'eaf-vue-demo)
+(require 'eaf-file-browser)
+(require 'eaf-netease-cloud-music)
+(require 'eaf-org-previewer)
+(require 'eaf-pdf-viewer)
+
+(provide 'init-eaf)
---
+++ lisp/init-keybindings.el
@@ -0,0 +1,14 @@
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+;; swap 2-indent or 4-indent
+(global-set-key (kbd "C-c t i") 'my-toggle-web-indent)
+
+;; 默认搜索当前被选中的或者在光标下的字符串
+(global-set-key (kbd "M-s o") 'occur-dwim)
+
+;; many cursors (just like sublime cursor)
+(global-set-key (kbd "M-s e") 'iedit-mode)
+
+
+(provide 'init-keybindings)
