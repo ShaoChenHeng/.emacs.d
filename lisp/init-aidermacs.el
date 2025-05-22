b127e3a815d8f249b59c854caa6cf6ed433787f3
shaochenheng <scheng52123@gmail.com>
2025-05-22 22:29:40 +0800

    Add aidermacs.

--- lisp/init-aidermacs.el
+++ lisp/init-aidermacs.el
@@ -1,10 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/aidermacs")
 (require 'aidermacs)
-
+(add-to-list 'exec-path "~/.local/bin")
 (global-set-key (kbd "C-c a") 'aidermacs-transient-menu)
-;; (add-hook 'aidermacs-before-run-backend-hook
-;;           (lambda ()
-;;             (setenv "DEEPSEEK_API_KEY" (getenv "ANTHROPIC_API_KEY_FROM_ENV"))))
+
 (setenv "DEEPSEEK_API_KEY" (getenv "DEEPSEEK_API_KEY_FROM_ENV"))
 (setq aidermacs-backend 'comint)
 (setq aidermacs-use-architect-mode t)
@@ -16,7 +14,5 @@
 
 ;; Optional: Set specific model for code generation
 (setq aidermacs-editor-model "deepseek/deepseek-chat")
-
-
 
 (provide 'init-aidermacs)
