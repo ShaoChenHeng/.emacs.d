4d4f5162159aca9d0e58fa5969073b1be6c68c06
shaochenheng <scheng52123@gmail.com>
2025-07-11 12:32:11 +0800

    change font

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -12,7 +12,7 @@
 (setq treemacs-follow-mode t
       treemacs-filewatch-mode t
       treemacs-fringe-indicator-mode 'always
-      treemacs-width 25)
+      treemacs-width 22)
 
 
 ;; keybinding
@@ -21,6 +21,21 @@
 (global-set-key (kbd "M-0") 'treemacs-select-window)
 (global-set-key (kbd "C-x t d") 'treemacs-select-directory)
 (global-set-key (kbd "C-x f") 'treemacs-find-file)
-;;(global-set-key (kbd "[mouse-1]") 'treemacs-single-click-expand-action)
+
+(defvar my-treemacs-font-family "hack"
+  "Font family used in Treemacs.")
+
+(with-eval-after-load 'treemacs
+  (set-face-attribute 'treemacs-directory-face nil :height 0.8 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-file-face nil :height 0.8 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-directory-collapsed-face nil :height 0.8 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-root-face nil :height 1.1 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-root-unreadable-face nil :height 1.1 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-git-modified-face nil :height 0.8 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-git-added-face nil :height 0.8 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-git-untracked-face nil :height 0.8 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-git-ignored-face nil :height 0.8 :family my-treemacs-font-family)
+  (set-face-attribute 'treemacs-async-loading-face nil :height 0.8 :family my-treemacs-font-family)
+)
 
 (provide 'init-treemacs)
