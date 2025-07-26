d3c290578c7d70ac62abdd3d56511dbe9a5f2fb4
shaochenheng <scheng52123@gmail.com>
2025-07-26 20:59:08 +0800

    set xx-ts-mode ident

--- lisp/init-programming.el
+++ lisp/init-programming.el
@@ -8,9 +8,21 @@
   )
 
 ;; js-ts-mode 缩进设置
+;; (add-hook 'js-ts-mode-hook
+;;           (lambda ()
+;;             (setq-local tab-width 2)
+;;             (setq-local js-indent-level 2)))
 (add-hook 'js-ts-mode-hook
           (lambda ()
-            (setq-local tab-width 2)
-            (setq-local js-indent-level 2)))
+            (setq-local js-indent-level 2)
+            (setq-local tab-width 2)))
+
+(add-hook 'java-ts-mode-hook
+          (lambda ()
+            (setq-local java-ts-mode-indent-offset 2)
+	    ))
+
+;; treesit- auto END
+
 
 (provide 'init-programming)
