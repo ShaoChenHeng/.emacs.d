7857d093e43deb2dfb021617181c726cac08ef69
shaochenheng <scheng52123@gmail.com>
2025-07-26 20:58:00 +0800

    smaller identation

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -14,7 +14,6 @@
       treemacs-fringe-indicator-mode 'always
       treemacs-width 22)
 
-
 ;; keybinding
 (global-set-key (kbd "C-x B") 'treemacs)
 (global-set-key (kbd "C-x t 1") 'treemacs-delete-other-windows)
@@ -24,6 +23,8 @@
 
 (defvar my-treemacs-font-family "hack"
   "Font family used in Treemacs.")
+
+(setq treemacs-indentation 1)
 
 (with-eval-after-load 'treemacs
   (set-face-attribute 'treemacs-directory-face nil :height 0.8 :family my-treemacs-font-family)
