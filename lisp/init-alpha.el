7cbb219494d9886633ecab99f0c2e0eedad68caa
shaochenheng <scheng52123@gmail.com>
2025-10-01 01:27:26 +0800

    remove window reset

--- lisp/init-alpha.el
+++ lisp/init-alpha.el
@@ -8,7 +8,7 @@
 ;;(set-frame-parameter (selected-frame) 'alpha-foreground 100) ; 文字不透明 100%
 
 ;; 定义 dashboard 专用的 alpha-background 值
-(defvar my-dashboard-alpha-bg 5)
+(defvar my-dashboard-alpha-bg 15)
 ;; 原主题的当前行高亮在透明dashboard中可见度太低，重新设置
 (defvar my-dashboard-hl-line-bg "#5a94f5")
 ;; 定义其他缓冲区的默认 alpha-background 值
@@ -27,23 +27,6 @@
 
 ;; 添加自动检测钩子
 (add-hook 'post-command-hook #'my-update-alpha-background)
-
-(let* ((screen-width (display-pixel-width))
-       (screen-height (display-pixel-height))
-       (char-width (frame-char-width))
-       (char-height (frame-char-height))
-       (new-width (max 1 (floor (/ (* screen-width 0.618) char-width))))
-       (new-height (max 1 (floor (/ (* screen-height 1) char-height))))
-
-       )
-
-  (with-eval-after-load 'dashboard
-    (setq initial-frame-alist
-          `((width . ,new-width)
-            (height . ,new-height) 
-            (left . 0)
-            (top . 0))))
-  )
 
 ;; 确保后续创建的窗口也继承设置
 (setq default-frame-alist initial-frame-alist)
