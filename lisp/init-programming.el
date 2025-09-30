1241d08ae052330cf41ea9735ff08dff3b6462b5
shaochenheng <scheng52123@gmail.com>
2025-10-01 01:29:55 +0800

    add region tab

--- lisp/init-programming.el
+++ lisp/init-programming.el
@@ -4,8 +4,7 @@
   (treesit-auto-install 'prompt)
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode)
-  )
+  (global-treesit-auto-mode))
 
 ;; js-ts-mode 缩进设置
 ;; (add-hook 'js-ts-mode-hook
@@ -19,10 +18,18 @@
 
 (add-hook 'java-ts-mode-hook
           (lambda ()
-            (setq-local java-ts-mode-indent-offset 2)
-	    ))
+            (setq-local java-ts-mode-indent-offset 2)))
+
+(defun my/python-ts-indent-region-or-tab ()
+  "在选区时整体缩进，否则插入 TAB 或调用缩进。"
+  (interactive)
+  (if (use-region-p)
+      (indent-region (region-beginning) (region-end))
+    (indent-for-tab-command)))
+
+(with-eval-after-load 'python-ts-mode
+  (define-key python-ts-mode-map (kbd "TAB") #'my/python-ts-indent-region-or-tab))
 
 ;; treesit- auto END
-
 
 (provide 'init-programming)
